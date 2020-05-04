c192b011e990d309348a3dad8b84e7f702e17cee
zanebliss <zanebliss@icloud.com>
2020-05-04 13:31:47 -0500

    init commit

---
+++ main.js
@@ -0,0 +1,33 @@
+let jumpStopRecords = []
+let chattenRecords = []
+let polarRecords = []
+
+const createFunkArtist = (name, age) => ({
+    name: name,
+    age: age
+})
+const createRapArtist = (name, age) => ({
+    name: name,
+    age: age
+})
+const createCountryArtist = (name, age) => ({
+    name: name,
+    age: age
+})
+const createBluegrassArtist = (name, age) => ({
+    name: name,
+    age: age
+})
+const createPopArtist = (name, age) => ({
+    name: name,
+    age: age
+})
+
+chattenRecords.push(createCountryArtist('Bruce Atikins', 23))
+polarRecords.push(createPopArtist('Jensen Brown'), 20)
+jumpStopRecords.push(createFunkArtist('Dre Funkz'), 25)
+jumpStopRecords.push(createRapArtist('Dusta Grimes'), 21)
+chattenRecords.push(createBluegrassArtist('Bartholomew Danielson'), 23)
+chattenRecords.push(createCountryArtist('Avilee Dallas'), 19)
+polarRecords.push(createPopArtist('Austin Kinkaid'), 22)
+jumpStopRecords.push(createRapArtist('Loyonce Branis'), 27)
